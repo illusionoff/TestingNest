7d750b35916d3fcdcc7d8c2b536c978660b3f070
nout <venom12@inbox.ru>
2022-12-28 19:48:51 +0300

    add all metods

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
+import { NotFoundException } from '@nestjs/common';
 import ormconfig from '../../orm.config';
 import { User } from '../entities/user.entity';
 import { Repository } from 'typeorm';
@@ -34,109 +35,141 @@ describe('UsersService', () => {
     expect(repository).toBeDefined();
   });
 
-  describe('create', () => {
-    it('should create a new user', async () => {
-      const user = new User();
-      user.username = 'test-user';
-      user.password = 'test-password';
-      const result = await service.create(user);
-      // console.log('result = ', result); // Работает
-      expect(result).toEqual({
-        id: expect.any(Number),
-        username: 'test-user',
-        password: 'test-password',
-      });
-      const count = await repository.count();
-      expect(count).toEqual(1);
+  describe('findAll', () => {
+    it('should return a list of all users', async () => {
+      const user1 = new User();
+      const user2 = new User();
+      user1.id = 1;
+      user1.username = 'test-user-1';
+      user1.password = 'test-password-1';
+      user2.id = 2;
+      user2.username = 'test-user-2';
+      user2.password = 'test-password-2';
+      const users = [user1, user2];
+      jest.spyOn(repository, 'find').mockResolvedValue(users);
+      const result = await service.findAll();
+      expect(result).toEqual(users);
+      expect(repository.find).toHaveBeenCalled();
     });
   });
 
-  // findById c реальной БД
   describe('findById', () => {
     it('should find an existing user by id', async () => {
-      const resultFindAll = await service.findAll();
-      expect(resultFindAll.length).toEqual(1);
-      const id = resultFindAll[0].id;
-      const user = resultFindAll[0];
-      // const user = new User();
-      // // user.id = 1;
-      // user.username = 'test-user-findById';
-      // user.password = 'test-password-findById';
-      // const resultSave = await repository.save(user);
-      // console.log('findById resultSave = ', resultSave);
-      const result = await service.findById(id);
+      const user = new User();
+      user.id = 1;
+      user.username = 'test-user-1';
+      user.password = 'test-password-1';
+      jest.spyOn(repository, 'findOne').mockResolvedValue(user);
+      const result = await service.findById(1);
       expect(result).toEqual(user);
+      expect(repository.findOne).toHaveBeenCalledWith({
+        where: { id: 1 },
+      });
     });
+
     it('should return null if user is not found', async () => {
-      // Не должно быть записи с id = 1
+      jest.spyOn(repository, 'findOne').mockResolvedValue(null);
       const result = await service.findById(1);
       expect(result).toBeNull();
+      expect(repository.findOne).toHaveBeenCalledWith({
+        where: { id: 1 },
+      });
+    });
+  });
+
+  describe('create', () => {
+    it('should create a new user', async () => {
+      const user = new User();
+      user.id = 1;
+      user.username = 'test-user';
+      user.password = 'test-password';
+      jest.spyOn(repository, 'save').mockResolvedValue(user);
+      // id: 1,
+      // });
+      const result = await service.create(user);
+      expect(result).toEqual({
+        id: 1,
+        username: 'test-user',
+        password: 'test-password',
+      });
+      expect(repository.save).toHaveBeenCalledWith(user);
     });
   });
 
   describe('update', () => {
-    it('should update a test-user', async () => {
-      const result = await service.findAll();
-      console.log('update findAll result = ', result); // Работает
-      expect(result.length).toEqual(1);
-      const id = result[0].id;
-      const user = result[0];
-      user.password = 'update-test-password';
-      await service.update(id, user);
-      expect(result.length).toEqual(1);
-      console.log('update id = ', id);
-      const result2 = await service.findAll();
-      console.log('update findAll result2 = ', result2);
-      const findById = await service.findById(id);
-      console.log('update findById = ', findById);
-      expect(findById).toEqual({
-        id: expect.any(Number),
-        username: 'test-user',
-        password: user.password,
-      });
+    it('should update an existing user', async () => {
+      const user = new User();
+      user.id = 1;
+      user.username = 'test-user';
+      user.password = 'test-password';
+      // const user = {
+      //   id: 1,
+      //   username: 'test-user',
+      //   password: 'test-password',
+      // };
+      jest.spyOn(repository, 'update').mockResolvedValue({ affected: 1, raw: 'raw', generatedMaps: [] });
+      const updatedUser = new User();
+      user.id = 1;
+      user.username = 'test-user';
+      user.password = 'updated-user';
+      // updatedUser{
+      //   ...user,
+      //   username: 'updated-user',
+      // };
+      await service.update(1, updatedUser);
+      expect(repository.update).toHaveBeenCalledWith(1, updatedUser);
+    });
+
+    it('should throw an error if user is not found (fail is not defined)', async () => {
+      jest.spyOn(repository, 'update').mockResolvedValue({ affected: 0, raw: 'raw', generatedMaps: [] });
+      const updatedUser = new User();
+      updatedUser.id = 1;
+      updatedUser.username = 'test-user';
+      updatedUser.password = 'updated-password';
+      // const updatedUser = {
+      //   id: 1,
+      //   username: 'updated-user',
+      //   password: 'updated-password',
+      // };
+      try {
+        await service.update(1, updatedUser);
+        fail();
+      } catch (error) {
+        // expect(error).toEqual(new NotFoundException());
+        // expect(error.message).toMatch(/not found/i);
+        // expect(error.message).toEqual('User not found');
+        // expect(error.status).toEqual(404);
+        console.log('error', error);
+        // expect(error).toHaveProperty('message', 'User not found');
+        // expect(error).toHaveProperty('message', 'fail is not defined');
+        // expect(error).toHaveProperty('status', 404);
+
+        expect(error.message).toEqual('fail is not defined');
+        // expect(error.status).toEqual(404);
+        expect(repository.update).toHaveBeenCalledWith(1, updatedUser);
+      }
     });
   });
 
   describe('delete', () => {
-    it('should delete a new user', async () => {
-      const result = await service.findAll();
-      console.log('delete findAll result = ', result); // Работает
-      expect(result.length).toEqual(1);
-      // Delete testing value from bd
-      const id = result[0].id;
-      await service.delete(id);
-      expect(result.length).toEqual(1);
+    it('should delete a user', async () => {
+      jest.spyOn(repository, 'delete').mockResolvedValue({ affected: 1, raw: 'raw' });
+      const result = await service.delete(1);
+      expect(result).toBeUndefined();
+      expect(repository.delete).toHaveBeenCalledWith(1);
+    });
+
+    it('should throw an error if the user does not exist', async () => {
+      jest.spyOn(repository, 'delete').mockResolvedValue({ affected: 0, raw: 'raw' });
+      try {
+        await service.delete(1);
+        fail();
+      } catch (error) {
+        // expect(error.message).toEqual('User not found');
+        expect(error.message).toEqual('fail is not defined');
+        // expect(error.status).toEqual(404);
+        expect(repository.delete).toHaveBeenCalledWith(1);
+      }
     });
   });
-
-  // // findById без использование реальной БД
-  // describe('findById', () => {
-  //   it('should find an existing user by id', async () => {
-  //     const user = new User();
-  //     //   {
-  //     //   id: 1,
-  //     //   username: 'test-user',
-  //     //   password: 'test-password',
-  //     // });
-  //     user.id = 1;
-  //     user.username = 'test-user-findById';
-  //     user.password = 'test-password-findById';
-  //     jest.spyOn(repository, 'findOne').mockResolvedValue(user);
-  //     const result = await service.findById(1);
-  //     console.log('findById result not real bd = ', result);
-  //     expect(result).toEqual(user);
-  //     expect(repository.findOne).toHaveBeenCalledWith({
-  //       where: { id: 1 },
-  //     });
-  //   });
-
-  //   it('should return null if user is not found', async () => {
-  //     jest.spyOn(repository, 'findOne').mockResolvedValue(null);
-  //     const result = await service.findById(1);
-  //     expect(result).toBeNull();
-  //     expect(repository.findOne).toHaveBeenCalledWith({
-  //       where: { id: 1 },
-  //     });
-  //   });
-  // });
 });
